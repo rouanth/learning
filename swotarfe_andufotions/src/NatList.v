c5540083e7aadb3489b2d84671e1514425012015
Dmitry Khalansky <rouanth@gmail.com>
2016-02-14 00:20:33 +0300

    Intermediate commit for ch. 3

--- swotarfe_andufotions/src/NatList.v
+++ swotarfe_andufotions/src/NatList.v
@@ -100,6 +100,58 @@ Proof. reflexivity. Qed.
 
 (* Exercise: 3 stars, advanced (alternate) *)
 
+Fixpoint alternate (l1 l2 : list nat) : list nat :=
+  match (l1, l2) with
+    | (l, nil) => l
+    | (nil, l) => l
+    | (cons h1 t1, cons h2 t2) => (h1 :: h2 :: alternate t1 t2)%list
+  end.
+
+Example test_alternate1:
+  alternate (1 :: 2 :: 3 :: nil)%list (4 :: 5 :: 6 :: nil)%list
+  = (1 :: 4 :: 2 :: 5 :: 3 :: 6 :: nil)%list.
+Proof. reflexivity. Qed.
+
+Example test_alternate2: alternate (1 :: nil)%list (4 :: 5 :: 6 :: nil)%list =
+  (1 :: 4 :: 5 :: 6 :: nil)%list.
+Proof. reflexivity. Qed.
+
+Example test_alternate3: alternate (1 :: 2 :: 3 :: nil)%list (4 :: nil)%list =
+  (1 :: 4 :: 2 :: 3 :: nil)%list.
+Proof. reflexivity. Qed.
+
+Example test_alternate4: alternate nil (20 :: 30 :: nil)%list
+  = (20 :: 30 :: nil)%list.
+Proof. reflexivity. Qed.
 
 (* END alternate. *)
+
+Definition bag := list nat.
+
+(* Exercise: 3 stars (bag_functions) *)
+
+Fixpoint count (v : nat) (s : bag) : nat :=
+  match s with
+    | nil => O
+    | cons h t => match beq_nat h v with
+                    | true  => S (count v t)
+                    | false => count v t
+                  end
+  end.
+
+Example test_count1: count 1 (1 :: 2 :: 3 :: 1 :: 4 :: 1 :: nil)%list = 3.
+Proof. reflexivity. Qed.
+
+Example test_count2: count 6 (1 :: 2 :: 3 :: 1 :: 4 :: 1 :: nil)%list = 0.
+Proof. reflexivity. Qed. 
+
+Definition bag_sum l1 l2 : bag := app  l1 l2.
+
+Example test_sum1 :
+  count 1 (bag_sum (1 :: 2 :: 3 :: nil)%list (1 :: 4 :: 1 :: nil)%list) = 3.
+Proof. reflexivity. Qed.
+
+Definition add (v : nat) (s : bag) : bag := cons v s.
+
+(* END bag_functions. *)
 
