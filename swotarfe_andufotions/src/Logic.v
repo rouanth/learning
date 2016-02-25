d4cc922c084af64a7e5a276a8e07b4bd4390bdb8
Dmitry Khalansky <rouanth@gmail.com>
2016-02-25 04:24:06 +0300

    and_assoc

--- swotarfe_andufotions/src/Logic.v
+++ swotarfe_andufotions/src/Logic.v
@@ -13,3 +13,23 @@ Qed.
 
 (* END proj2. *)
 
+(* Exercise: 2 stars (and_assoc) *)
+
+Theorem and_assoc : forall P Q R : Prop,
+  P /\ (Q /\ R) -> (P /\ Q) /\ R.
+Proof.
+  intros P Q R H.
+  destruct H as [HP [HQ HR]].
+  split.
+  Case "P and Q".
+    split.
+    SCase "P".
+      apply HP.
+    SCase "Q".
+      apply HQ.
+  Case "R".
+    apply HR.
+Qed.
+
+(* END and_assoc. *)
+
