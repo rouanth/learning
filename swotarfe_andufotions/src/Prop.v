63ada95ef2343eed1509743d0e5f9d7119b54035
Dmitry Khalansky <rouanth@gmail.com>
2016-02-28 11:52:53 +0300

    double_even

---
+++ swotarfe_andufotions/src/Prop.v
@@ -0,0 +1,23 @@
+Require Export Logic.
+
+(* ((Inductively Defined Propositions)) *)
+
+Inductive ev : nat -> Prop :=
+  | ev_O  : ev 0
+  | ev_SS : forall n : nat, ev n -> ev (S (S n)).
+
+(* Exercise: 1 star (double_even) *)
+
+Theorem double_even: forall n, ev (double n).
+Proof.
+  induction n as [|n'].
+  Case "n = 0".
+    simpl.
+    apply ev_O.
+  Case "n = S n'".
+    simpl.
+    apply ev_SS.
+    apply IHn'.
+Qed.
+
+(* END double_even. *)
