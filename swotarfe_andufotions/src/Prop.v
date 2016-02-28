923404146fa9b631863f76436e6665ad57e5e5eb
Dmitry Khalansky <rouanth@gmail.com>
2016-02-28 12:04:05 +0300

    b_times2

--- swotarfe_andufotions/src/Prop.v
+++ swotarfe_andufotions/src/Prop.v
@@ -28,3 +28,23 @@ Qed.
 
 (* END varieties_of_beauty. *)
 
+Inductive beautiful : nat -> Prop :=
+  | bu_0   : beautiful 0
+  | bu_3   : beautiful 3
+  | bu_5   : beautiful 5
+  | bu_sum : forall n m : nat, beautiful n -> beautiful m -> beautiful (n + m).
+
+(* Exercise: 2 stars (b_times2) *)
+
+Theorem b_times2 : forall n, beautiful n -> beautiful (2 * n).
+Proof.
+  intros n H.
+  simpl.
+  rewrite -> plus_0_r.
+  apply bu_sum.
+  apply H.
+  apply H.
+Qed.
+
+(* END b_times2. *)
+
