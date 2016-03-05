72d3fc96de0e03c5b097525aa9003f2bc76fbf23
Dmitry Khalansky <rouanth@gmail.com>
2016-03-05 14:16:14 +0300

    dist_not_exists

--- swotarfe_andufotions/src/MoreLogic.v
+++ swotarfe_andufotions/src/MoreLogic.v
@@ -8,3 +8,16 @@ There exists such natural number n that the following number has property
 
 (* END english_exists. *)
 
+(* Exercise: 1 star (dist_not_exists) *)
+
+Theorem dist_not_exists : forall (X : Type) (P : X -> Prop),
+  (forall x, P x) -> not (exists x, not (P x)).
+Proof.
+  unfold not.
+  intros X P H1 H2.
+  inversion H2.
+  apply H.
+  apply H1.
+Qed.
+
+(* END dist_not_exists. *)
