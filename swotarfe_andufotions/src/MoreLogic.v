1cdac5793a85e6b4c3e8863727dd56f73ce86e7a
Dmitry Khalansky <rouanth@gmail.com>
2016-03-05 14:11:38 +0300

    english_exists

---
+++ swotarfe_andufotions/src/MoreLogic.v
@@ -0,0 +1,10 @@
+Require Export PropL.
+
+(* Exercise: 1 star, optional (english_exists) *)
+
+(* ex nat (fun n => beautiful (S n)) means:
+There exists such natural number n that the following number has property
+"beautiful" *)
+
+(* END english_exists. *)
+
