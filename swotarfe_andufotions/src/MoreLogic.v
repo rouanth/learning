c8d42112eeb0850405ae463f3f165aade1ccf061
Dmitry Khalansky <rouanth@gmail.com>
2016-03-05 14:32:57 +0300

    not_exists_dist

--- swotarfe_andufotions/src/MoreLogic.v
+++ swotarfe_andufotions/src/MoreLogic.v
@@ -21,3 +21,22 @@ Proof.
 Qed.
 
 (* END dist_not_exists. *)
+
+(* Exercise: 3 stars, optional (not_exists_dist) *)
+
+Theorem not_exists_dist : excluded_middle -> forall (X : Type) (P : X -> Prop),
+  not (exists x, not (P x)) -> (forall x, P x).
+Proof.
+  unfold not.
+  intros EM X P H x.
+  apply excluded_middle_implies_classic in EM.
+  unfold classic in EM.
+  unfold not in EM.
+  apply EM.
+  intros pxf.
+  apply H.
+  exists x.
+  apply pxf.
+Qed.
+
+(* END not_exists_dist. *)
