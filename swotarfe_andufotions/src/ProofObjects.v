23930ccbcafba947f24657a74e72862321afd057
Dmitry Khalansky <rouanth@gmail.com>
2016-03-07 21:24:08 +0300

    nine_is_beautiful

--- swotarfe_andufotions/src/ProofObjects.v
+++ swotarfe_andufotions/src/ProofObjects.v
@@ -13,3 +13,15 @@ Qed.
 Definition six_is_beautiful' := bu_sum 3 3 bu_3 bu_3.
 
 (* END six_is_beautiful. *)
+
+(* Exercise: 1 star (nine_is_beautiful) *)
+
+Theorem nine_is_beautiful :
+  beautiful 9.
+Proof.
+  apply (bu_sum 3 6 bu_3 (bu_sum 3 3 bu_3 bu_3)).
+Qed.
+
+Definition nine_is_beautiful' := bu_sum 3 6 bu_3 (bu_sum 3 3 bu_3 bu_3).
+
+(* END nine_is_beautiful. *)
