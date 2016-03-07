fe5086d7e6beaeb98f13561c522d8d47da2dc776
Dmitry Khalansky <rouanth@gmail.com>
2016-03-07 22:06:53 +0300

    b_times2

--- swotarfe_andufotions/src/ProofObjects.v
+++ swotarfe_andufotions/src/ProofObjects.v
@@ -25,3 +25,12 @@ Qed.
 Definition nine_is_beautiful' := bu_sum 3 6 bu_3 (bu_sum 3 3 bu_3 bu_3).
 
 (* END nine_is_beautiful. *)
+
+(* ((Quantification, Implications and Functions)) *)
+
+(* Exercise: 2 stars b_times2 *)
+
+Definition b_times2' : forall (n : nat), beautiful n -> beautiful (2 * n) :=
+  fun n => fun E => bu_sum n (n + 0) E (bu_sum n 0 E bu_0).
+
+(* END b_times2. *)
