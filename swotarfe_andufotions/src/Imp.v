1f4f5ec8cd5dde0dc75e26604d683db650e26f92
rouanth <rouanth@gmail.com>
2016-03-23 17:41:47 +0300

    update_neq

--- swotarfe_andufotions/src/Imp.v
+++ swotarfe_andufotions/src/Imp.v
@@ -267,3 +267,17 @@ Proof.
 Qed.
 
 (* END update_eq. *)
+
+(* Exercise: 1 star (update_neq) *)
+
+Theorem update_neq : forall x2 x1 n st,
+  x2 <> x1 ->
+  (update st x2 n) x1 = (st x1).
+Proof.
+  intros.
+  unfold update.
+  apply neq_id.
+  apply H.
+Qed.
+
+(* END update_neq. *)
