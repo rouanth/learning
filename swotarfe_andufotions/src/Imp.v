b7087738eabedb0cbae10a1637aa5a003ec6b3df
rouanth <rouanth@gmail.com>
2016-03-23 18:56:30 +0300

    update_permute

--- swotarfe_andufotions/src/Imp.v
+++ swotarfe_andufotions/src/Imp.v
@@ -320,3 +320,18 @@ Proof.
 Qed.
 
 (* END update_same. *)
+
+(* Exercise: 3 stars (update_permute) *)
+
+Theorem update_permute : forall n1 n2 x1 x2 x3 st,
+  x2 <> x1 ->
+  (update (update st x2 n1) x1 n2) x3 = (update (update st x1 n2) x2 n1) x3.
+Proof.
+  unfold update.
+  intros.
+  destruct (eq_id_dec x1 x3).
+  subst. symmetry. apply neq_id. apply H.
+  reflexivity.
+Qed.
+
+(* END update_permute. *)
