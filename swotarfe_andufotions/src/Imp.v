0efcf5fbbab25460f1b17c28175ea99c4b5c1a61
rouanth <rouanth@gmail.com>
2016-03-23 17:17:53 +0300

    neq_id

--- swotarfe_andufotions/src/Imp.v
+++ swotarfe_andufotions/src/Imp.v
@@ -233,3 +233,17 @@ Proof.
 Qed.
 
 (* END bevalR. *)
+
+(* Exercise: 1 star, optional (neq_id) *)
+
+Lemma neq_id : forall (T : Type) x y (p q:T), x <> y ->
+               (if eq_id_dec x y then p else q) = q.
+Proof.
+  unfold not.
+  intros.
+  destruct (eq_id_dec x y).
+    contradiction.
+  trivial.
+Qed.
+
+(* END neq_id. *)
