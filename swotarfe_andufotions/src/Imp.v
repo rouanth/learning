6bc3574685228815524a91275aa64395b41504dd
rouanth <rouanth@gmail.com>
2016-04-09 14:42:18 +0300

    pup_to_n

--- swotarfe_andufotions/src/Imp.v
+++ swotarfe_andufotions/src/Imp.v
@@ -486,3 +486,36 @@ Proof.
 Qed.
 
 (* END ceval_example2. *)
+
+(* Exercise: 3 stars, advanced (pup_to_n) *)
+
+Definition pup_to_n : com :=
+  Y ::= ANum 0;;
+  WHILE (BNot (BEq (ANum 0) (AId X))) DO
+    Y ::= APlus  (AId X) (AId Y);;
+    X ::= AMinus (AId X) (ANum 1)
+  END.
+
+Theorem pup_to_2_ceval :
+  pup_to_n / (update empty_state X 2) ⇓
+    update (update (update (update (update (update empty_state
+      X 2) Y 0) Y 2) X 1) Y 3) X 0.
+Proof.
+  unfold pup_to_n.
+  apply E_Seq with (update (update empty_state X 2) Y 0).
+  apply E_Ass.
+  apply E_WhileLoop with (update (update (update (update empty_state
+    X 2) Y 0) Y 2) X 1).
+  reflexivity.
+  apply E_Seq with (update (update (update empty_state X 2) Y 0) Y 2);
+    apply E_Ass.
+  apply E_WhileLoop with (update (update (update (update (update (update
+    empty_state X 2) Y 0) Y 2) X 1) Y 3) X 0).
+  reflexivity.
+  apply E_Seq with (update (update (update (update (update
+    empty_state X 2) Y 0) Y 2) X 1) Y 3); apply E_Ass.
+  apply E_WhileEnd.
+  reflexivity.
+Qed.
+
+(* END pup_to_n. *)
