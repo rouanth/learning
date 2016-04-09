126306b7abbba79d7c9bb868298037f75dbb01fe
rouanth <rouanth@gmail.com>
2016-04-09 15:09:17 +0300

    loop_never_stops

--- swotarfe_andufotions/src/Imp.v
+++ swotarfe_andufotions/src/Imp.v
@@ -538,3 +538,17 @@ Proof.
 Qed.
 
 (* END XtimesYinZ_spec. *)
+
+(* Exercise: 3 stars (loop_never_stops) *)
+
+Theorem loop_never_stops : forall st st',
+  ~(loop / st ⇓ st').
+Proof.
+  intros st st' contra. unfold loop in contra.
+  remember (WHILE BTrue DO SKIP END) as loopdef eqn:Heqloopdef.
+  induction contra; inversion Heqloopdef; subst.
+  inversion H.
+  apply IHcontra2. trivial.
+Admitted.
+
+(* END loop_never_stops. *)
