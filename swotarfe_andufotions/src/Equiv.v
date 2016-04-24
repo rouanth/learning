3084b564b0ff471bdcb2dccf774e2da8d57c2dbe
Dmitry Khalansky <rouanth@gmail.com>
2016-04-24 15:52:01 +0300

    skip_right

--- swotarfe_andufotions/src/Equiv.v
+++ swotarfe_andufotions/src/Equiv.v
@@ -10,3 +10,20 @@ Require Export Imp.
 ] *)
 
 (* END equiv_classes. *)
+
+Definition cequiv (c1 c2 : com) :=
+  forall st st', c1 / st ⇓ st' <-> c2 / st ⇓ st'.
+
+(* Exercise: 2 stars (skip_right) *)
+
+Theorem skip_right: forall c, cequiv (c ;; SKIP) c.
+Proof.
+  intros c st st'.
+  split.
+  Case "SKIP -> c".
+    intros. inversion H. inversion H5. subst. assumption.
+  Case "c -> SKIP".
+    intros. apply E_Seq with (st' := st'). assumption. apply E_Skip.
+Qed.
+
+(* END skip_right. *)
