987acdd8856cc009cc0f0f05c63e39c79161831b
Dmitry Khalansky <rouanth@gmail.com>
2016-04-24 15:48:01 +0300

    equiv_classes

---
+++ swotarfe_andufotions/src/Equiv.v
@@ -0,0 +1,12 @@
+Require Export Imp.
+
+(* Exercise: 2 stars (equiv_classes) *)
+
+(* [
+  [a, d, f, g], -- do not terminate
+  [b, e],       -- y ::= 0
+  [c, h],       -- SKIP
+  [i]           -- if x < y, then x = y; else loop
+] *)
+
+(* END equiv_classes. *)
