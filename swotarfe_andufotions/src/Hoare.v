7eb5b528e33b49c0be9dc7fb47d3164f62ef1100
Dmitry Khalansky <rouanth@gmail.com>
2016-06-19 21:54:51 +0300

    Started chapter Hoare

---
+++ swotarfe_andufotions/src/Hoare.v
@@ -0,0 +1,10 @@
+Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import SfLib.
+Require Import Imp.
+
+Definition Assertion := state -> Prop.
+
+
