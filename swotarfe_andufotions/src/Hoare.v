304a602f478cd1e2c474c6224b77c886df900721
Dmitry Khalansky <rouanth@gmail.com>
2016-06-20 22:28:00 +0300

    assertions

--- swotarfe_andufotions/src/Hoare.v
+++ swotarfe_andufotions/src/Hoare.v
@@ -7,4 +7,32 @@ Require Import Imp.
 
 Definition Assertion := state -> Prop.
 
+(* Exercise: 1 star, optional (assertions) *)
+
+Module ExAssertions.
+Definition as1 : Assertion := fun st => st X = 3.
+(* X = 3 *)
+
+Definition as2 : Assertion := fun st => st X <= st Y.
+(* X ≤ Y *)
+
+Definition as3 : Assertion :=
+          fun st => st X = 3 \/ st X <= st Y.
+(* Either X = 3 or X ≤ Y *)
+
+Definition as4 : Assertion :=
+          fun st => st Z * st Z <= st X /\
+                      ~ (((S (st Z)) * (S (st Z))) <= st X).
+(* Z² ≤ X or not (Z² ≤ X), i. e. always true *)
+
+Definition as5 : Assertion := fun st => True.
+(* Any state matches *)
+
+Definition as6 : Assertion := fun st => False.
+(* No states match *)
+
+End ExAssertions.
+
+
+(* END assertions. *)
 
