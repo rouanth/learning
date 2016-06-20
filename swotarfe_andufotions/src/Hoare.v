5338c38e1caf43f06ec12ce05694340dbe635ae7
Dmitry Khalansky <rouanth@gmail.com>
2016-06-20 22:40:59 +0300

    triples

--- swotarfe_andufotions/src/Hoare.v
+++ swotarfe_andufotions/src/Hoare.v
@@ -36,3 +36,52 @@ End ExAssertions.
 
 (* END assertions. *)
 
+Definition assert_implies (P Q : Assertion) : Prop :=
+  forall st, P st -> Q st.
+
+Notation "P ->> Q" := (assert_implies P Q)
+        (at level 80) : hoare_spec_scope.
+Open Scope hoare_spec_scope.
+
+Notation "P <<->> Q" := (P ->> Q /\ Q ->> Q) (at level 80) : hoare_spec_scope.
+
+Definition hoare_triple
+        (P : Assertion) (c : com) (Q : Assertion) : Prop :=
+        forall st st',
+        c / st ⇓ st' ->
+        P st ->
+        Q st.
+
+Notation "{{ P }} c {{ Q }}" := (hoare_triple P c Q) (at level 90,
+        c at next level) : hoare_spec_scope.
+
+(* Exercise: 1 star, optional (triples) *)
+
+(*
+   1) {{True}} c {{X = 5}}
+   For all initial states, after `c` X is equal to 5.
+
+   2) {{X = m}} c {{X = m + 5)}}
+   After `c` X increases by 5.
+
+   3) {{X ≤ Y}} c {{Y ≤ X}}
+   If X was less or equal to Y before `c`, then after it this relation
+   inverses.
+
+   4) {{True}} c {{False}}
+   `c` never terminates.
+
+   5) {{X = m}}
+      c
+      {{Y = real_fact m}}    
+   `c` assigns to Y the result of applying `real_fact` to X.
+
+   6) {{True}}
+      c
+      {{(Z * Z) ≤ m ∧ ¬ (((S Z) * (S Z)) ≤ m)}}
+
+    `c` never terminates.
+*)
+
+(* END triples. *)
+
