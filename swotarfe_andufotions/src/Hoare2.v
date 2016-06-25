2e1782c8f62c5e353058961ca492f98e17c52951
Dmitry Khalansky <rouanth@gmail.com>
2016-06-25 22:53:09 +0300

    Started chapter Hoare2

---
+++ swotarfe_andufotions/src/Hoare2.v
@@ -0,0 +1,7 @@
+Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import SfLib.
+Require Import Imp.
+Require Import Hoare.
