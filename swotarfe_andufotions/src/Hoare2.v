2ee67c8ccf4ff546f85be95d6befe16163ea2bc2
Dmitry Khalansky <rouanth@gmail.com>
2016-07-23 19:34:48 +0300

    if_minus_plus_reloaded

--- swotarfe_andufotions/src/Hoare2.v
+++ swotarfe_andufotions/src/Hoare2.v
@@ -5,3 +5,23 @@ Require Import Coq.omega.Omega.
 Require Import SfLib.
 Require Import Imp.
 Require Import Hoare.
+
+(* Exercise: 2 stars (if_minus_plus_reloaded) *)
+
+(*
+   {{ True }}
+  IFB X ≤ Y THEN
+      {{ True /\ X ≤ Y           }} ⇾
+      {{ Y = X + (Y - X)         }}
+    Z ::= Y - X
+      {{ Y = X + Z               }}
+  ELSE
+      {{ True /\ ~ (X ≤ Y)       }} ⇾
+      {{ X + Z = X + Z           }}
+    Y ::= X + Z
+      {{ Y = X + Z               }}
+  FI
+    {{ Y = X + Z }}
+*)
+
+(* END if_minus_plus_reloaded. *)
