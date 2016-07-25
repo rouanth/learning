f58d293c7cf6884337be3fdda82fe60ad5395abb
Dmitry Khalansky <rouanth@gmail.com>
2016-07-25 10:40:08 +0300

    redo_determinism

--- swotarfe_andufotions/src/Smallstep.v
+++ swotarfe_andufotions/src/Smallstep.v
@@ -10,6 +10,8 @@ Inductive tm : Type :=
   | C : nat -> tm
   | P : tm  -> tm  -> tm.
 
+Module SimpleArith1.
+
 Reserved Notation " t '=>' t' " (at level 40).
 
 Inductive step : tm -> tm -> Prop :=
@@ -41,3 +43,38 @@ Proof. apply ST_Plus2. apply ST_Plus2. apply ST_PlusConstConst. Qed.
 (* Proof. repeat constructor. Qed. *)
 
 (* END test_step_2. *)
+
+End SimpleArith1.
+
+Inductive value : tm -> Prop :=
+  | v_const : forall n, value (C n).
+
+Reserved Notation "t '=>' t'" (at level 40).
+
+Inductive step : tm -> tm -> Prop :=
+  | ST_PlusConstConst : forall n1 n2,
+      P (C n1) (C n2) => C (n1 + n2)
+  | ST_Plus1 : forall t1 t1' t2,
+      t1 => t1' ->
+      P t1 t2 => P t1' t2
+  | ST_Plus2 : forall v1 t2 t2',
+      value v1 ->
+      t2 => t2' ->
+      P v1 t2 => P v1 t2'
+  where "t '=>' t'" := (step t t').
+
+(* Exercise: 3 stars, recommended (redo_determinism) *)
+
+Theorem step_deterministic :
+  deterministic step.
+Proof.
+  unfold deterministic. intros x y1 y2 Hind.
+  generalize dependent y2.
+  induction Hind; intros; inversion H; subst; try solve by inversion 2;
+    trivial.
+  - rewrite IHHind with t1'0; trivial.
+  - inversion H0; subst; try solve by inversion 2.
+    rewrite IHHind with t2'0; trivial.
+Qed.
+
+(* END redo_determinism. *)
