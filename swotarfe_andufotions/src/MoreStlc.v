90d3ccf29bb5207467c777e9c4b149e2e04cad49
Dmitry Khalansky <rouanth@gmail.com>
2016-08-13 18:14:07 +0300

    halve_fix

--- swotarfe_andufotions/src/MoreStlc.v
+++ swotarfe_andufotions/src/MoreStlc.v
@@ -2,3 +2,15 @@ Require Import SfLib.
 Require Import Types.
 Require Import Smallstep.
 Require Import Stlc.
+
+(* Exercise: 1 star, optional (halve_fix) *)
+
+(*
+halve_fix = fix (\f : Nat -> Nat.
+              forall x : Nat.
+                if x == 0 then 0 else
+                if pred x == 0 then 0 else
+                1 + f (pred (pred x)))
+*)
+
+(* END halve_fix. *)
